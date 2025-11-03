42a2b83337cb4effdf63947cde1a7739147206fd
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-11-03 22:08:31 +0200

    create clean text that remove spaces and html tags, but not specail characters becuase the job description or resume may have normal special characters

---
+++ resume_tailoring_website/resume_tailoring_service/utils/validations.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+
+
+
+
+def clean_text(text):
+
+
+    soup = BeautifulSoup(text, 'html.parser')
+    text = soup.get_text()
+
+    text = ' '.join(text.split())
+
+    return text
+
+
