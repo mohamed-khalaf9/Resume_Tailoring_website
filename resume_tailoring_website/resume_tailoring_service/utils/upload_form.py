a9c26a7d6b21c4be94096fd14ba959f0e2ac3e7e
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-11-03 21:26:08 +0200

    add some constraints over fields, job details and resume

--- resume_tailoring_website/resume_tailoring_service/utils/upload_form.py
+++ resume_tailoring_website/resume_tailoring_service/utils/upload_form.py
@@ -4,5 +4,13 @@ from django import forms
 
 
 class UploadForm(forms.Form):
-    job_description = forms.CharField(widget=forms.Textarea)
-    resume_pdf_file = forms.FileField()
+    job_description = forms.CharField(
+        widget=forms.Textarea,max_length = 6000,
+        min_length= 1000,strip =True,label='The Job Description',
+        required=True,
+        help_text='Enter your job details here...', )
+
+    resume_pdf_file = forms.FileField(
+        label='The Resume File(PDF)',required=True,
+        help_text='Please upload a PDF file under 5MB.',
+        max_length=100, )
