4a3670ab978f3ea6a8c651f769d0870c013b0278
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-11-03 19:40:59 +0200

    create object from UploadForm to display it on template, accept form submitted data and extract them, and this impl for testing purposes now

--- resume_tailoring_website/resume_tailoring_service/views.py
+++ resume_tailoring_website/resume_tailoring_service/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .utils import upload_form
+from .utils.upload_form import UploadForm
 
 
 def upload(request):
-    return render(request,'resume_tailoring/welcome.html')
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            job_description = form.cleaned_data['job_description']
+            resume_pdf_file = form.cleaned_data['resume_pdf_file']
+            print(type(resume_pdf_file))#debugging
+            return
+        else:
+            form = UploadForm()
+
+
+    else:
+        form = UploadForm()
+
+        return render(request,'resume_tailoring/welcome.html',{'form':form})
+
+
