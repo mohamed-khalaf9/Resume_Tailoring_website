b5df388f44936bffeacb4ae2fde6d17e7726ac7a
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-11-03 22:21:43 +0200

    validate resume file extenstion and file size

--- resume_tailoring_website/resume_tailoring_service/utils/upload_form.py
+++ resume_tailoring_website/resume_tailoring_service/utils/upload_form.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.validators import FileExtensionValidator
+from django.core.exceptions import ValidationError
 
 from resume_tailoring_website.resume_tailoring_service.utils.validations import clean_text
 
@@ -13,12 +15,25 @@ class UploadForm(forms.Form):
     resume_pdf_file = forms.FileField(
         label='The Resume File(PDF)',required=True,
         help_text='Please upload a PDF file under 5MB.',
-        max_length=100, )
+        max_length=100,
+        validators=[FileExtensionValidator(['pdf'])]
+
+    )
 
 
     def clean_job_description(self):
         job_description = self.cleaned_data['job_description']
         job_description = clean_text(job_description)
         return job_description
+
+# check difference between .get and square bracket in dictionary to access value
+    def clean_resume_pdf_file(self):
+        resume_pdf_file = self.cleaned_data['resume_pdf_file']
+        MAX_SIZE_MB = 6
+        if resume_pdf_file.size > MAX_SIZE_MB * 1024 * 1024:
+            raise ValidationError(f"Resume file must not exceed {MAX_SIZE_MB} MB")
+
+        return resume_pdf_file
+
 
 
