f823333bb4e7f0a31f8272a27f021e36f882a74f
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-11-05 22:17:07 +0200

    call tailor resume to test the LLM calling

--- resume_tailoring_website/resume_tailoring_service/views.py
+++ resume_tailoring_website/resume_tailoring_service/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from .utils.LLM_integration import tailor_resume
 from .utils.pdf_processing import extract_resume_content
 from .utils.upload_form import UploadForm
 
@@ -12,8 +13,13 @@ def upload(request):
             job_description = form.cleaned_data['job_description']
             resume_pdf_file = form.cleaned_data['resume_pdf_file']
             all_links_details,resume_content = extract_resume_content(resume_pdf_file.read())
+            print("Resume Content########################################")
             print(resume_content)
+            print("HyperLinkks############################################")
             print(all_links_details)
+            tailored_resume_content = tailor_resume(resume_content,all_links_details,job_description)
+            print("Tailored Resume Content############################################")
+            print(tailored_resume_content)
 
         else:
             form = UploadForm()
