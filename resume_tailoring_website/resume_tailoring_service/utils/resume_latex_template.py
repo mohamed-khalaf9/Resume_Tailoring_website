425363f49e9948ffe77c38d31e187b95a0e11898
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-11-07 15:01:59 +0200

    complete education section in latex template by filling the related fields in ResumeContent modle

--- resume_tailoring_website/resume_tailoring_service/utils/resume_latex_template.py
+++ resume_tailoring_website/resume_tailoring_service/utils/resume_latex_template.py
@@ -8,7 +8,9 @@ def build_resume(resume: ResumeContent):
     resume_template = r"""
     
 \begin{document}
-    
+
+%-----------PERSONAL INFORMATION-----------  
+
 \begin{center}
 
 {% if personal_info.name %}
@@ -40,6 +42,65 @@ def build_resume(resume: ResumeContent):
 {% endfor %}
 {% endif %}
 \end{center}
+
+%-----------EDUCATION-----------
+
+
+{% if education %}
+\section{Education}
+\resumeSubHeadingListStart
+{% for education_item in education %}
+    \resumeSubheading
+      { {% if education_item.university_name %}
+          {{ education_item.university_name }} University
+        {% endif %} }
+      
+      { {%- if education_item.country and education_item.city -%}
+          {{ education_item.country }}, {{ education_item.city }}
+        {%- elif education_item.country -%}
+          {{ education_item.country }}
+        {%- elif education_item.city -%}
+          {{ education_item.city }}
+        {%- endif -%} 
+      }
+      
+      {
+        {%- if education_item.degree and education_item.faculty and education_item.specialization -%}
+          {{ education_item.degree }} of {{ education_item.faculty }} in {{ education_item.specialization }}
+        {%- elif education_item.degree and education_item.faculty and not education_item.specialization -%}
+          {{ education_item.degree }} of {{ education_item.faculty }}
+        {%- elif education_item.degree and education_item.specialization and not education_item.faculty -%}
+          {{ education_item.degree }} in {{ education_item.specialization }}
+        {%- elif education_item.faculty and education_item.specialization -%}
+          {{ education_item.faculty }} in {{ education_item.specialization }}
+        {%- elif education_item.degree -%}
+          {{ education_item.degree }}
+        {%- elif education_item.faculty -%}
+          {{ education_item.faculty }}
+        {%- elif education_item.specialization -%}
+          {{ education_item.specialization }}
+        {%- endif -%}
+      }
+      
+
+      { {%- if education_item.start_date and education_item.end_date -%}
+          {{ education_item.start_date }} -- {{ education_item.end_date }}
+        {%- elif education_item.start_date -%}
+          {{ education_item.start_date }}
+        {%- else -%}
+          {{ education_item.end_date }}
+        {%- endif -%} 
+      }
+
+    {% if education_item.related_coursework %}
+      \resumeItemListStart
+        \resumeItem{\textbf{Related Coursework:} {{ education_item.related_coursework | join(", ") }} }
+      \resumeItemListEnd
+    {% endif %}
+
+{% endfor %}
+ \resumeSubHeadingListEnd
+{% endif %}
 
 
     
