7525b83cccf6f16fe07540c98dc055ca219a5f61
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-11-08 17:44:27 +0200

    add url of pdf download view function

--- resume_tailoring_website/resume_tailoring_service/urls.py
+++ resume_tailoring_website/resume_tailoring_service/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('tailor', upload, name='tailor'),
     path('preview', preview, name='preview'),
     path('preview/pdf', pdf_preview, name="serve_pdf_preview"),
+    path('preview/pdf/download', pdf_download, name="serve_pdf_download"),
 
 ]
